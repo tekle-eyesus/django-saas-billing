830de3166eb52a54c09efd99c81dae5787792165
tekle-eyesus <tekleeyesus21@gmail.com>
2025-12-10 06:56:12 -0700

    Register Transaction in Admin

--- apps/billing/admin.py
+++ apps/billing/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('tx_ref', 'user', 'amount', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('tx_ref', 'user__email', 'organization__name')
+    readonly_fields = ('tx_ref', 'amount', 'currency', 'created_at')
